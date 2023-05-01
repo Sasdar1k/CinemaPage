58e48e7dfd08ed86957595818333a2cd20e51bff
Sasdar1k <123847481+Sasdar1k@users.noreply.github.com>
2023-05-01 16:07:57 +0300

    Push
    Первая версия страницы

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Initialize Swiper
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 'auto',
+  spaceBetween: 0,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
